0cc975b7fe7770a786232cacf214c2e4861e177a
Yunhee Kang <yunh.kang@gmail.com>
2020-04-21 12:18:24 +0900

    실습

--- Openweathermap_JSON_except.py
+++ Openweathermap_JSON_except.py
@@ -1,35 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
 import requests
 
 
-# https://code-maven.com/openweathermap-api-using-python
-#https://stackoverflow.com/questions/46992156/parsing-data-in-dictionary-json-using-openweathermap-api
+# In[18]:
+
+
 def search_city(city):
+
     API_KEY = 'a070fcd8fc2db8d5d1f140466a2012b4'  # initialize your key here
     # call API and convert response into Python dictionary
-
+    
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_KEY}'
     response = requests.get(url).json()
 
-    #print (response)
+
     # error like unknown city name, inavalid api key
     if response.get('cod') != 200:
         message = response.get('message', '')
         return f'Error getting temperature for {city.title()}. Error message = {message}'
 
-    description = response['weather'][0]['description']
-    print( f'Current weather description of {city.title()} is {description}') 
-
-    pressure = response['main']['pressure']
-    print( f'Current pressure of {city.title()} is {pressure}') 
-
-    current_humidity = response.get('main', {}).get('humidity')
-    if current_humidity:
-        print( f'Current humidity of {city.title()} is {current_humidity}')
-    else:
-        print( f'Error getting humidity for {city.title()}')
-    
     # get current temperature and convert it into Celsius
-    # city.title() Make the first letter in each word upper case
     current_temperature = response.get('main', {}).get('temp')
     if current_temperature:
         current_temperature_celsius = round(current_temperature - 273.15, 2)
@@ -37,14 +32,31 @@ def search_city(city):
     else:
         return f'Error getting temperature for {city.title()}'
 
-  
-while True:
-    #print("도시이름을 입력해 주세요")
-    city = input("도시이름을 입력해 주세요 : ")
-    if city == 'Nocity':
-        break
+
+# In[23]:
+
+
+city = input("Enter city name:")
+try: 
+    for char in city:
+        if char in "0123456789":
+            raise ValueError
+except ValueError:
+    print ("Invalid city name")
+    #exit()
+else:
+    print("city name %s" % city)
     result = search_city(city)
     print(result)
+
+
+# In[22]:
+
+
+result
+
+
+# In[ ]:
 
 
 
